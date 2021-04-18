8378b117d87acce10a64c47d67dde9899daedef4
Jacobsma <mitch5736@gmail.com>
2021-04-18 02:33:01 -0400

    Standardized code

--- src/appx_synch.py
+++ src/appx_synch.py
@@ -15,7 +15,7 @@ class AutomataCore:
         self._compute_automaton_m()
 
     @property
-    def transitionFunction(self):
+    def transitionFunction(self) -> list:
         return self._transitionFunction
 
     @transitionFunction.setter
@@ -24,7 +24,7 @@ class AutomataCore:
         return 
 
     @property
-    def powerTransitionFunction(self):
+    def powerTransitionFunction(self) -> list:
         return self._powerTransitionFunction
 
     @powerTransitionFunction.setter
@@ -42,7 +42,7 @@ class AutomataCore:
         return
 
     @property
-    def stateToKey(self,state:tuple):
+    def stateToKey(self,state:tuple) -> int:
         return self._stateToKey[state]
 
     @stateToKey.setter
@@ -51,7 +51,7 @@ class AutomataCore:
         return
 
     @property
-    def keyToState(self,key:int):
+    def keyToState(self,key:int) -> tuple:
         return self._keyToState[key]
 
     @keyToState.setter
@@ -60,10 +60,10 @@ class AutomataCore:
         return
 
     @property
-    def keyToStateDict(self):
+    def keyToStateDict(self) -> dict:
         return self._keyToState
 
-    def _compute_image(self,letter:int,tup:tuple) -> tuple:
+    def _compute_image(self,letter:list,tup:tuple) -> tuple:
         im = list()
         for state in tup:
             im.append(letter[state])
@@ -97,18 +97,18 @@ class Automata(AutomataCore):
         self._graph = dict()
 
     def graph(self,transition:list):
-      self._graph = dict([(i, dict()) for i in range(len(transition[0]))])
-      symbols = [chr(ord('a') + i) for i in range(len(self._weights))]
-      for letter, weight, symbol in sorted(zip(transition, self._weights, symbols), key = lambda x: x[1], reverse=True):
-        i = 0
-        for el in letter:
-          self._graph[el][i] = (weight, symbol)
-          i = i+1
-      return self._graph 
+        self._graph = dict([(i, dict()) for i in range(len(transition[0]))])
+        symbols = [chr(ord('a') + i) for i in range(len(self._weights))]
+        for letter, weight, symbol in sorted(zip(transition, self._weights, symbols), key = lambda x: x[1], reverse=True):
+            i = 0
+            for el in letter:
+                self._graph[el][i] = (weight, symbol)
+                i = i+1
+        return 
 
 
     #TODO: maybe store this?
-    def compute_shortest_words(self,starting_vertex):
+    def compute_shortest_words(self,starting_vertex:int) -> dict:
         distances = {vertex: (float('infinity'),"") for vertex in self._graph}
         distances[starting_vertex] = (0,"")
 
@@ -128,93 +128,95 @@ class Automata(AutomataCore):
 
         return distances
 
-    def compute_image_by_word(self, tup, word):
-      result = tup
-      for symbol in word:
-        letter = self._transitionFunction[ord(symbol) - 97]
-        result = self._compute_image(letter, result)
-      return result
+    def compute_image_by_word(self, tup:tuple, word:str) -> tuple:
+        result = tup
+        for symbol in word:
+            letter = self._transitionFunction[ord(symbol) - 97]
+            result = self._compute_image(letter, result)
+        return result
 
-    def compute_shortest_word(self):
-      self._power = len(self._transitionFunction[0])
-      self._compute_automaton_m()
-      aut = (self.powerTransitionFunction,self.keyToStateDict) 
-      graph = self.graph(aut[0])
-      shortest_words = self.compute_shortest_words(0)
-      for i in range(1,len(self._transitionFunction[0])):
-        tmp = self.compute_shortest_words(i)
-        for el in tmp:
-          if shortest_words[el][0] > tmp[el][0]:
-            shortest_words[el] = tmp[el]
-      return list(shortest_words.items())[-1][1]
-
-
-    def t1(self,P,T,m):
-      return P.issubset(set(T)) and len(P) > 1
-
-    def H1(self,P,T,w):
-      image = self.compute_image_by_word(P, w[1])
-      return w[0]/(len(P)-len(image))
-
-    def H2(self,P,T,w):
-      image = self.compute_image_by_word(T, w[1])
-      return w[0]/(len(T)-len(image))
-
-    def t3(self,P,T,m):
-      return set(P).issubset(set(T)) and len(P) == min(m, len(T))
-
-    def H3(self,P,T,w):
-     return  w[0]
-
-    def H4(self,P,T,w):
-      image = self.compute_image_by_word(T, w[1])
-      return w[0]/((len(T)-len(image)) ** 2)
+    def compute_shortest_word(self) -> list:
+        self._power = len(self._transitionFunction[0])
+        self._compute_automaton_m()
+        aut = (self.powerTransitionFunction,self.keyToStateDict) 
+        graph = self.graph(aut[0])
+        shortest_words = self.compute_shortest_words(0)
+        for i in range(1,len(self._transitionFunction[0])):
+            tmp = self.compute_shortest_words(i)
+            for el in tmp:
+                if shortest_words[el][0] > tmp[el][0]:
+                    shortest_words[el] = tmp[el]
+        return list(shortest_words.items())[-1][1]
 
 
-    def word_weight(self,word):
-      weight = 0
-      for letter in word:
-        weight = weight + self._weights[ord(letter) - 97]
-      return weight
+    def t1(self,P:set,T:tuple,m:int) -> bool:
+        return P.issubset(set(T)) and len(P) > 1
+
+    def H1(self,P:set,T:tuple,w:str) -> float:
+        image = self.compute_image_by_word(P, w[1])
+        return w[0]/(len(P)-len(image))
+
+    def H2(self,P:set,T:tuple,w:str) -> float:
+        image = self.compute_image_by_word(T, w[1])
+        return w[0]/(len(T)-len(image))
+
+    def t3(self,P:set,T:tuple,m:int) -> bool:
+        return set(P).issubset(set(T)) and len(P) == min(m, len(T))
+
+    def H3(self,P:set,T:tuple,w:str) -> int:
+        return  w[0]
+
+    def H4(self,P:set,T:tuple,w:str) -> float:
+        image = self.compute_image_by_word(T, w[1])
+        return w[0]/((len(T)-len(image)) ** 2)
 
 
-    def approximate_weighted_synch(self,m, t, H):
-      self._power = m
-      self._compute_automaton_m()
-      aut = (self.powerTransitionFunction,self.keyToStateDict) 
-      graph = self.graph(aut[0])
-      shortest_words_with_inf = self.compute_shortest_words(0)
-      shortest_words = dict()
-      for i in range(1,len(self._transitionFunction[0])):
-        tmp_words = self.compute_shortest_words(i)
-        for el in tmp_words:
-          if shortest_words_with_inf[el][0] > tmp_words[el][0]:
-            shortest_words_with_inf[el] = tmp_words[el]
-      for el in shortest_words_with_inf:
-        if shortest_words_with_inf[el][0] != float('infinity'):
-          shortest_words[el] = shortest_words_with_inf[el]
-      T = tuple(range(len(self._transitionFunction[0])))
-      u = ""
-      while len(T) > 1:
-        current_shortest = float('infinity')
-        w = ""
-        for el in shortest_words:
-          P = set(aut[1][el])
-          if t(P, T, m):
-            tmp = H(P,T,shortest_words[el])
-            if tmp < current_shortest:
-              current_shortest = tmp
-              w = shortest_words[el][1]
-        if w == "":
-          return ""
-        u = u + w
-        T = self.compute_image_by_word(T, w)
-      return (u, len(u), self.word_weight(u))
+    def word_weight(self,word:str) -> int:
+        weight = 0
+        for letter in word:
+            weight = weight + self._weights[ord(letter) - 97]
+        return weight
+
+
+    def approximate_weighted_synch(self,m:int, t, H) -> tuple:
+        self._power = m
+        self._compute_automaton_m()
+        aut = (self.powerTransitionFunction,self.keyToStateDict) 
+        self.graph(aut[0])
+        shortest_words_with_inf = self.compute_shortest_words(0)
+        shortest_words = dict()
+        for i in range(1,len(self._transitionFunction[0])):
+            tmp_words = self.compute_shortest_words(i)
+            for el in tmp_words:
+                if shortest_words_with_inf[el][0] > tmp_words[el][0]:
+                    shortest_words_with_inf[el] = tmp_words[el]
+        for el in shortest_words_with_inf:
+            if shortest_words_with_inf[el][0] != float('infinity'):
+                shortest_words[el] = shortest_words_with_inf[el]
+        T = tuple(range(len(self._transitionFunction[0])))
+        u = ""
+        while len(T) > 1:
+            current_shortest = float('infinity')
+            w = ""
+            for el in shortest_words:
+                P = set(aut[1][el])
+                if t(P, T, m):
+                    tmp = H(P,T,shortest_words[el])
+                    if tmp < current_shortest:
+                        current_shortest = tmp
+                        w = shortest_words[el][1]
+            if w == "":
+                return ""
+            u = u + w
+            T = self.compute_image_by_word(T, w)
+        return (u, len(u), self.word_weight(u))
     
     def __str__(self):
         return f"\n----Automata----\n\n\nTransition Function:\n{self._transitionFunction}\n\nWeights:\n{self._weights}\n\nPower:\n{self._power}\n\nPower Transition Function:\n{self._powerTransitionFunction}\n\nGraph:\n{self._graph}\n\nKeys To States:\n{self.keyToStateDict}\n\n----------------\n"
 
 if __name__ == "__main__":
+
+    """
     mTwo = Automata([[1,2,3,4,5,6,7,8,9,10,11,0],[0,1,2,3,4,5,6,7,8,9,10,0], [1,1,2,3,4,5,6,7,8,9,10,11], [0,1,2,3,3,5,6,7,8,9,10,11]],2, [1,6,2,7]) 
     print("m = 2")
     print("H1" ,mTwo.approximate_weighted_synch(2,mTwo.t1,mTwo.H1))
@@ -250,3 +252,14 @@ if __name__ == "__main__":
 
     print("least weight" ,c, len(c[1]))
     print("shortest" ,d, len(d[1]))
+    """
+    
+    test = Automata([[1,2,3,0],[0,1,2,0],[0,1,0,3]],2,[1,2,6])
+    print("H1", test.approximate_weighted_synch(4,test.t1,test.H1))
+    print("H2", test.approximate_weighted_synch(4,test.t1,test.H2))
+    print("H3", test.approximate_weighted_synch(4,test.t3,test.H3))
+    print("H4", test.approximate_weighted_synch(4,test.t1,test.H4))
+    print("Shortest", test.compute_shortest_word())
+    print(test)
+
+
